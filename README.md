cf270d122010d8d2e573baee6c0f5d15c2221a74
Alazar Zewdu <alazarzewdu3@gmail.com>
2023-01-24 10:33:50 +0300

    Finished Project

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Unity University Library System
+
+This is a system that manages books in the Unity University library
+
+
+## Authors (Section N4 Students)
+
+- [@Alazar Zewdu] UU79133R(https://www.github.com/octokatherine)
+
+
+## Demo
+
+Insert gif or link to demo
+
+
+## Acknowledgements
+
+ - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
+ - [Awesome README](https://github.com/matiassingers/awesome-readme)
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
---
+++ src/components/Book.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+
+const Book = ({
+  id,
+  bookname,
+  author,
+  shelf,
+  quantity,
+  date,
+  handleRemoveBook
+}) => {
+  const history = useHistory();
+
+  return (
+    <Card style={{ width: '18rem' }} className="book">
+      <Card.Body>
+        <Card.Title className="book-title">{bookname}</Card.Title>
+        <div className="book-details">
+          <div>Author: {author}</div>
+          <div>Quantity: {quantity} </div>
+          <div>shelf: {shelf} </div>
+          <div>Date: {new Date(date).toDateString()}</div>
+        </div>
+        <Button variant="primary" onClick={() => history.push(`/edit/${id}`)}>
+          Edit
+        </Button>{' '}
+        <Button variant="danger" onClick={() => handleRemoveBook(id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Book;
